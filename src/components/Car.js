3ec6cef56ee977fe52e197fc00aa5c3fdf936ccf
unknown <mamlamdia29@gmail.com>
2023-02-19 15:38:30 -0800

    Ajout du fichier Car.js

---
+++ src/components/Car.js
@@ -0,0 +1,12 @@
+import { tab } from "@testing-library/user-event/dist/tab"
+import { useState } from "react"
+import { useEffect } from "react"
+
+function Car() {
+    const monsteraPrice = 8
+    const [car, updateCart] = useState(0)
+    const [isOpen, setIsOpen] = useState(0) 
+
+}
+       
+export default Car 
